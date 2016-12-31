f9eb0215eedd51c0f743edcc3f6361a557f85af8
jontelarsson94 <jonathan-larsson@outlook.com>
2016-12-31 11:27:57 +0100

    started with email controller

---
+++ Volito Automation app/HydroSwede/HydroSwede/EmailController.swift
@@ -0,0 +1,38 @@
+//
+//  EmailController.swift
+//  HydroSwede
+//
+//  Created by Jonathan Larsson on 2016-12-31.
+//  Copyright © 2016 jonathan larsson. All rights reserved.
+//
+
+import UIKit
+
+class EmailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let blueColor = UIColor(red: 192/255.0, green: 192/255.0, blue: 192/255.0, alpha: 1.0)
+        view.backgroundColor = blueColor
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
